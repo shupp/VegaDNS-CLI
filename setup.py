67480c941c34a35e476eafedb40a224d888a9554
Bill Shupp <hostmaster@shupp.org>
2015-06-03 16:19:02 -0700

    Renamed vdns.py vdns, added initial setup.py for distribution

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='vegadns_cli',
+      version='0.1.0',
+      description='VegaDNS CLI tool',
+      # long_description=open('README.md').read(),
+      url='https://github.com/shupp/VegaDNS',
+      author='Bill Shupp',
+      author_email='hostmaster@shupp.org',
+      license='Apache 2.0',
+      install_requires=[
+        "click==4.0",
+        "pep8==1.6.2",
+        "requests==2.7.0",
+      ],
+      packages=[
+        'vegadns_cli',
+        'vegadns_cli.commands',
+        'vegadns_client',
+        'vegadns_client.store'
+    ],
+      scripts=['vdns'],
+      zip_safe=False)
