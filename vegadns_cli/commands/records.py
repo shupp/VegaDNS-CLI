42924f1fb55b84e944204c6b7c0c3c6c6cca4f21
Bill Shupp <hostmaster@shupp.org>
2015-06-22 18:00:26 -0700

    Added create_aaaa_record

--- vegadns_cli/commands/records.py
+++ vegadns_cli/commands/records.py
@@ -81,6 +81,49 @@ def create_a_record(ctx, domain_id, name, ip, ttl=3600):
     help="TTL of the record to create, defaults to 3600"
 )
 @click.option(
+    "--ip",
+    type=unicode,
+    prompt=True,
+    help="IPv6 address of the record to create, required"
+)
+@click.option(
+    "--name",
+    type=unicode,
+    prompt=True,
+    help="Hostname of the record to create, required"
+)
+@click.option(
+    "--domain-id",
+    type=int,
+    prompt=True,
+    help="ID of the domain to list records for, required"
+)
+@click.pass_context
+def create_aaaa_record(ctx, domain_id, name, ip, ttl=3600):
+    try:
+        data = {
+            "record_type": "AAAA",
+            "domain_id": domain_id,
+            "name": name,
+            "value": ip,
+            "ttl": ttl
+        }
+        record = ctx.obj['client'].records.create(data)
+        click.echo(json.dumps(record.values, indent=4))
+    except ClientException as e:
+        click.echo("Error: " + str(e.code))
+        click.echo("Response: " + e.message)
+        ctx.exit(1)
+
+
+@cli.command()
+@click.option(
+    "--ttl",
+    type=int,
+    prompt=False,
+    help="TTL of the record to create, defaults to 3600"
+)
+@click.option(
     "--value",
     type=unicode,
     prompt=True,
